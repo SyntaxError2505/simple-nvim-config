c9003030796fe3734586b5b73b02e05222f0de63
sascha cherevko <mr.sascha.cherevko@gmail.com>
2026-02-03 17:20:08 +0100

    added diagnostics with custom icons

--- lua/sascha/settings.lua
+++ lua/sascha/settings.lua
@@ -5,4 +5,26 @@ o.tabstop = 4
 o.number = true
 o.relativenumber = true
 
+vim.diagnostic.config(
+    {
+        underline = false,
+        virtual_text = {
+            spacing = 2,
+            prefix = "●",
+        },
+        update_in_insert = false,
+        severity_sort = true,
+        signs = {
+            text = {
+                -- Alas nerdfont icons don't render properly on Medium!
+                [vim.diagnostic.severity.ERROR] = " ", 
+                [vim.diagnostic.severity.WARN] = " ",
+                [vim.diagnostic.severity.HINT] = " ",
+                [vim.diagnostic.severity.INFO] = " ",
+            },
+        },
+    }
+)
+
+
 vim.cmd.colorscheme("catppuccin-macchiato")
