f083636bc88df8b69eb0e39e9bd1bf024b2f13a2
Sascha Cherevko <mr.sascha.cherevko@gmail.com>
2026-02-10 22:15:37 +0100

    added markdown preview

---
+++ lua/sascha/plugins/markdown-preview.lua
@@ -0,0 +1,10 @@
+return {
+  "iamcco/markdown-preview.nvim",
+  cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+  build = "cd app && yarn install",
+  init = function()
+    vim.g.mkdp_filetypes = { "markdown" }
+	vim.g.mkpd_port = '8060'
+  end,
+  ft = { "markdown" },
+}
